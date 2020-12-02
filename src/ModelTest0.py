be9fb6fd68b22876f023ca269ca91dd261cf60c6
briny-fish <1179102664@qq.com>
2020-12-02 21:24:20 +0800

    akjsdfa

--- src/ModelTest0.py
+++ src/ModelTest0.py
@@ -3,34 +3,54 @@ import matchzoo as mz
 import nltk
 import pandas as pd
 from loader import *
+
 #nltk.download('punkt')
-print('-------------')
+W2V, W2V_VOCAB, word2ix,W2V_value = get_W2V()
+#print(W2V_VOCAB[:10])
+#print(W2V_value[:10])
+#print('-------------')
 event_file = '/Users/wit/OneDrive - mail.scut.edu.cn/njusearch/data/news pairs/raw/event-story-cluster/same_event_doc_pair.txt'
-print('-------------')
+#print('-------------')
 stopwords_file = '/Users/wit/OneDrive - mail.scut.edu.cn/njusearch/data/news pairs/raw/event-story-cluster/stopwords-zh.txt'
-print('-------------')
+#print('-------------')
 d,datas = read_input(event_file,stopwords_file)
-print('-------------')
+#print('-------------')
 Datas = pd.DataFrame(datas)
 Datas['label']=Datas['label'].astype(int)
-print(Datas['label'])
+#print(Datas['label'])
 class_task = mz.tasks.Classification(num_classes = 2)
 class_task.metrics = ['acc']
 class_task.losses = ['cross_entropy']
 
 data_pack = mz.pack(Datas,'classification')
-print(data_pack.frame()['label'])
-train = data_pack.frame()[data_pack.frame()['id_left'] <= ("L-%s" % int(29063 * 0.8))]
-valid = data_pack.frame()[data_pack.frame()['id_left'] > ("L-%s" % int(29063 * 0.8))]
+#print(data_pack.frame()['label'])
+train = data_pack.frame()[:int(29063*0.8)]
+valid = data_pack.frame()[int(29063*0.8):]
 # 定义数据
-
+print(train)
+print(len(valid))
 train_pack = mz.pack(train,'classification')
 valid_pack = mz.pack(valid,'classification')
-print(train_pack.frame()['label'])
-preprocessor = mz.models.ESIM.get_default_preprocessor()
+#print(train_pack.frame()['label'])
+preprocessor = mz.models.ESIM.get_default_preprocessor(truncated_mode='post',truncated_length_left=200,truncated_length_right=200)
+#truncated_mode=post，截取truncatedlenth的内容，pre的话就是从后面删掉这么长的内容
 train_processed = preprocessor.fit_transform(train_pack)
 valid_processed = preprocessor.transform(valid_pack)
-print(train_processed.frame()['text_left'][:10])
+#print(train_processed.frame()['text_left'][:10])
+print(len(train_processed.frame()))
+print(len(valid_processed.frame()))
+tmp = train_processed.frame()
+valid_out = None
+cnt = 0
+print(preprocessor.context['vocab_unit'].state.keys())
+for i in range(10):
+    for j in range(len(tmp['text_left'].iloc[i])):
+        print(tmp['text_left'].iloc[i][j])
+        if(preprocessor.context['vocab_unit'].state['index_term'][tmp['text_left'].iloc[i][j]] in W2V_VOCAB):
+            tmp['text_left'].iloc[i][j]=word2ix[preprocessor.context['vocab_unit'].state['index_term'][tmp['text_left'].iloc[i][j]]]
+        else:
+            tmp['text_left'].iloc[i][j]=2
+        print(W2V_VOCAB[tmp['text_left'].iloc[i][j]])
 #print(len(train_processed[train_processed.frame()['label']==1]))
 #print(train_processed.frame()[:2]['text_right'])
 trainset = mz.dataloader.Dataset(
@@ -44,7 +64,7 @@ validset = mz.dataloader.Dataset(
     batch_size=64
 )
 
-padding_callback = mz.models.ESIM.get_default_padding_callback()
+padding_callback = mz.models.ESIM.get_default_padding_callback(pad_word_mode='post')
 
 trainloader = mz.dataloader.DataLoader(
     dataset=trainset,
@@ -57,8 +77,9 @@ validloader = mz.dataloader.DataLoader(
     callback=padding_callback
 )
 
-
 model = mz.models.ESIM()
+model.params['embedding']=np.array(W2V_value)
+model.embedding = model._make_default_embedding_layer()
 model.params['task'] = class_task
 model.params['embedding_output_dim'] = 100
 model.params['embedding_input_dim'] = preprocessor.context['embedding_input_dim']
@@ -77,4 +98,4 @@ trainer = mz.trainers.Trainer(
     epochs=10
 )
 
-trainer.run()
+#trainer.run()
--- src/loader.py
+++ src/loader.py
@@ -2,7 +2,10 @@ import numpy as np
 import pandas as pd
 import tqdm
 import sys
-
+import pickle
+import gensim
+import os
+from collections import OrderedDict
 def read_stopwords(infile):
     res = {}
     for line in open(infile,encoding='utf-8'):
@@ -50,14 +53,81 @@ def read_input(infile, stopwords):
         datasdict['text_left'].append(fields[5].strip())
         datasdict['text_right'].append(fields[6].strip())
     return datas,datasdict
+
+def load_w2v(fin, type, vector_size):
+    """
+    Load word vector file.
+    :param fin: input word vector file name.
+    :param type: word vector type, "Google" or "Glove" or "Company".
+    :param vector_size: vector length.
+    :return: Output Gensim word2vector model.
+    """
+    model = {}
+    if type == "Google" or type == "Glove":
+        model = gensim.models.KeyedVectors.load_word2vec_format(
+            fin, binary=True)
+    elif type == "Company":
+        model["<PAD>"] = np.zeros(vector_size)
+        model["<OOV>"] = np.random.uniform(-0.25, 0.25, vector_size)
+        with open(fin, "r", encoding="utf-8") as fread:
+            for line in fread.readlines():
+                line_list = line.strip().split(" ")
+                word = line_list[0]
+                word_vec = np.fromstring(" ".join(line_list[1:]),
+                                         dtype=float, sep=" ")
+                model[word] = word_vec
+    else:
+        print("type must be Glove or Google or Company.")
+        sys.exit(1)
+    print(type)
+    return model
+def transform_w2v(W2V, vector_size):
+    W2V = dict((k, W2V[k]) for k in W2V.keys()
+               if len(W2V[k]) == vector_size)
+    W2V = OrderedDict(W2V)
+    W2V_value = [W2V[k] for k in W2V.keys()]
+    W2V_VOCAB = W2V.keys()
+    W2V_VOCAB = [w for w in W2V_VOCAB]
+    word2ix = {word: i for i, word in enumerate(W2V)}
+    return W2V, W2V_VOCAB, word2ix,W2V_value
+def load_W2V_VOCAB(language):
+    if language == "Chinese":
+        print("load w2v vocabulary ...")
+        W2V_VOCAB_PKL_FILE = "/Users/wit/OneDrive - mail.scut.edu.cn/njusearch/data/news pairs/raw/word2vec/w2v-zh.vocab.pkl"
+        if not os.path.exists(W2V_VOCAB_PKL_FILE):
+            W2V = load_w2v("/Users/wit/OneDrive - mail.scut.edu.cn/njusearch/data/news pairs/raw/word2vec/w2v-zh.model",
+                           "Company", 200)
+            W2V_VOCAB = set(W2V.keys())  # must be a set to accelerate remove_OOV
+            pickle.dump(W2V_VOCAB, open(W2V_VOCAB_PKL_FILE, "wb"))
+        else:
+            W2V_VOCAB = pickle.load(open(W2V_VOCAB_PKL_FILE, "rb"))
+        return W2V_VOCAB
+    elif language == "English":
+        print("load w2v vocabulary ...")
+        W2V_VOCAB_PKL_FILE = "../../../../data/raw/Google-w2v/GoogleNews-vectors-negative300.vocab.pkl"
+        if not os.path.exists(W2V_VOCAB_PKL_FILE):
+            W2V = load_w2v("../../../../data/raw/Google-w2v/GoogleNews-vectors-negative300.bin",
+                           "Google", 300)
+            W2V_VOCAB = set(W2V.wv.vocab.keys())  # must be a set to accelerate remove_OOV
+            pickle.dump(W2V_VOCAB, open(W2V_VOCAB_PKL_FILE, "wb"))
+        else:
+            W2V_VOCAB = pickle.load(open(W2V_VOCAB_PKL_FILE, "rb"))
+        return W2V_VOCAB
+
+def get_W2V():
+    W2V = load_w2v("/Users/wit/OneDrive - mail.scut.edu.cn/njusearch/data/news pairs/raw/word2vec/w2v-zh.model",
+                           "Company", 200)
+    return transform_w2v(W2V,200)
 if __name__ == '__main__':
     event_file = '/Users/wit/OneDrive - mail.scut.edu.cn/njusearch/data/news pairs/raw/event-story-cluster/same_event_doc_pair.txt'
     stopwords_file = '/Users/wit/OneDrive - mail.scut.edu.cn/njusearch/data/news pairs/raw/event-story-cluster/stopwords-zh.txt'
 
-    stopwords = read_stopwords(stopwords_file)
-    d1,d2 = event_datas = read_input(event_file, stopwords)
+    #stopwords = read_stopwords(stopwords_file)
+    #d1,d2 = event_datas = read_input(event_file, stopwords)
     # story_datas = read_input(story_file, stopwords)
-    Dframe = pd.DataFrame(d2)
-    print(Dframe[:10])
-    print(len(Dframe))
+    #Dframe = pd.DataFrame(d2)
+    #print(Dframe[:10])
+    #print(len(Dframe))
     #print(event_datas)
+    W2V = load_w2v("/Users/wit/OneDrive - mail.scut.edu.cn/njusearch/data/news pairs/raw/word2vec/w2v-zh.model",
+                           "Company", 200)
