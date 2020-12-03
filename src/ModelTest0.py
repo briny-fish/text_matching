66eb00195cb0bd76669b196ffc292b41b61d357d
briny-fish <1179102664@qq.com>
2020-12-03 19:51:13 +0800

    alksdjf

--- src/ModelTest0.py
+++ src/ModelTest0.py
@@ -1,11 +1,42 @@
 import torch
 import matchzoo as mz
+from matchzoo import preprocessors
 import nltk
+import typing
 import pandas as pd
 from loader import *
+class new_pre(preprocessors.BasicPreprocessor):
+    def __init__(self,
+                 truncated_mode: str = 'pre',
+                 truncated_length_left: int = None,
+                 truncated_length_right: int = None,
+                 filter_mode: str = 'df',
+                 filter_low_freq: float = 1,
+                 filter_high_freq: float = float('inf'),
+                 remove_stop_words: bool = False,
+                 ngram_size: typing.Optional[int] = None):
+        """Initialization."""
+        super().__init__(truncated_mode=truncated_mode,
+            truncated_length_left=truncated_length_left,
+            truncated_length_right=truncated_length_right,
+            filter_mode=filter_mode,
+            filter_low_freq=filter_low_freq,
+            filter_high_freq=filter_high_freq,
+            remove_stop_words=remove_stop_words,
+            ngram_size=ngram_size)
+    @classmethod
+    def _default_units(cls) -> list:
+        """Prepare needed process units."""
+        return [
+            mz.preprocessors.units.tokenize.Tokenize(),
+            mz.preprocessors.units.punc_removal.PuncRemoval(),
+        ]
 
 #nltk.download('punkt')
 W2V, W2V_VOCAB, word2ix,W2V_value = get_W2V()
+print(len(W2V_value))
+print(len(W2V_VOCAB))
+print(len(word2ix))
 #print(W2V_VOCAB[:10])
 #print(W2V_value[:10])
 #print('-------------')
@@ -16,50 +47,53 @@ stopwords_file = '/Users/wit/OneDrive - mail.scut.edu.cn/njusearch/data/news pai
 d,datas = read_input(event_file,stopwords_file)
 #print('-------------')
 Datas = pd.DataFrame(datas)
+print(len(Datas))
 Datas['label']=Datas['label'].astype(int)
 #print(Datas['label'])
 class_task = mz.tasks.Classification(num_classes = 2)
 class_task.metrics = ['acc']
 class_task.losses = ['cross_entropy']
+sent_len = 200
+print(Datas.iloc[1])
+print(len(Datas))
+print(Datas.iloc[0])
 
-data_pack = mz.pack(Datas,'classification')
-#print(data_pack.frame()['label'])
-train = data_pack.frame()[:int(29063*0.8)]
-valid = data_pack.frame()[int(29063*0.8):]
-# 定义数据
-print(train)
-print(len(valid))
-train_pack = mz.pack(train,'classification')
-valid_pack = mz.pack(valid,'classification')
 #print(train_pack.frame()['label'])
-preprocessor = mz.models.ESIM.get_default_preprocessor(truncated_mode='post',truncated_length_left=200,truncated_length_right=200)
+#preprocessor = new_pre(truncated_mode='post',truncated_length_left=200,truncated_length_right=200)
 #truncated_mode=post，截取truncatedlenth的内容，pre的话就是从后面删掉这么长的内容
-train_processed = preprocessor.fit_transform(train_pack)
-valid_processed = preprocessor.transform(valid_pack)
+#train_processed = preprocessor.fit_transform(data_pack)
+#print(len(train_processed.frame()))
+#tmp = train_processed.frame()
+#valid_out = None
+#cnt = 0
+
+#print(len(preprocessor.context['vocab_unit'].state['index_term']))
+#vocab_size = len(preprocessor.context['vocab_unit'].state['index_term'])
+#Max = 0
+#index2term = preprocessor.context['vocab_unit'].state['index_term']
+#term2index = preprocessor.context['vocab_unit'].state['term_index']
+
+valid_processed = Datas[int(29063*0.8):]
+train_processed = Datas[:int(29063*0.8)]
+
+# 定义数据
+#print(train)
+#print(len(valid))
+train_pack = mz.pack(train_processed,'classification')
+train_pack.append_text_length(inplace=True, verbose=1)
+valid_pack = mz.pack(valid_processed,'classification')
+valid_pack.append_text_length(inplace=True, verbose=1)
 #print(train_processed.frame()['text_left'][:10])
-print(len(train_processed.frame()))
-print(len(valid_processed.frame()))
-tmp = train_processed.frame()
-valid_out = None
-cnt = 0
-print(preprocessor.context['vocab_unit'].state.keys())
-for i in range(10):
-    for j in range(len(tmp['text_left'].iloc[i])):
-        print(tmp['text_left'].iloc[i][j])
-        if(preprocessor.context['vocab_unit'].state['index_term'][tmp['text_left'].iloc[i][j]] in W2V_VOCAB):
-            tmp['text_left'].iloc[i][j]=word2ix[preprocessor.context['vocab_unit'].state['index_term'][tmp['text_left'].iloc[i][j]]]
-        else:
-            tmp['text_left'].iloc[i][j]=2
-        print(W2V_VOCAB[tmp['text_left'].iloc[i][j]])
-#print(len(train_processed[train_processed.frame()['label']==1]))
-#print(train_processed.frame()[:2]['text_right'])
+print(len(train_pack.frame()))
+print(len(valid_pack.frame()))
+
 trainset = mz.dataloader.Dataset(
-    data_pack=train_processed,
+    data_pack=train_pack,
     mode='point',
     batch_size=64
 )
 validset = mz.dataloader.Dataset(
-    data_pack=valid_processed,
+    data_pack=valid_pack,
     mode='point',
     batch_size=64
 )
@@ -82,7 +116,7 @@ model.params['embedding']=np.array(W2V_value)
 model.embedding = model._make_default_embedding_layer()
 model.params['task'] = class_task
 model.params['embedding_output_dim'] = 100
-model.params['embedding_input_dim'] = preprocessor.context['embedding_input_dim']
+model.params['embedding_input_dim'] = len(W2V_VOCAB)
 model.guess_and_fill_missing_params()
 model.build()
 print(model.params)
@@ -95,7 +129,7 @@ trainer = mz.trainers.Trainer(
     optimizer=optimizer,
     trainloader=trainloader,
     validloader=validloader,
-    epochs=10
+    epochs=30
 )
 
 #trainer.run()
--- src/loader.py
+++ src/loader.py
@@ -15,12 +15,14 @@ def read_stopwords(infile):
 
 
 def read_input(infile, stopwords):
+    W2V, W2V_VOCAB, word2ix,W2V_value = get_W2V()
     lineid = 0
     field_num = 0
     datas = []
     docid = 0
-    datasdict = {'text_right':[],'text_left':[],'label':[]}
+    datasdict = {'id_left':[],'text_right':[],'id_right':[],'text_left':[],'label':[]}
     for line in open(infile,encoding='utf-8'):
+        print(lineid)
         lineid += 1
         if lineid == 1:
             field_num = len(line.split('|'))
@@ -37,7 +39,7 @@ def read_input(infile, stopwords):
         cur_data['doc1']['orig'] = fields[5].strip()
         cur_data['doc1']['text'] = cur_data['doc1']['orig'].replace(' ', '')
         cur_data['doc1']['tokens'] = cur_data['doc1']['orig'].split(' ')
-        cur_data['doc1']['tokens_without_stopwords'] = [w for w in cur_data['doc1']['tokens'] if w not in stopwords]
+        cur_data['doc1']['tokens_without_stopwords'] = [word2ix[w] for w in cur_data['doc1']['tokens'] if w not in stopwords]
         cur_data['doc1']['docid'] = docid
         docid += 1
 
@@ -45,13 +47,15 @@ def read_input(infile, stopwords):
         cur_data['doc2']['orig'] = fields[6].strip()
         cur_data['doc2']['text'] = cur_data['doc2']['orig'].replace(' ', '')
         cur_data['doc2']['tokens'] = cur_data['doc2']['orig'].split(' ')
-        cur_data['doc2']['tokens_without_stopwords'] = [w for w in cur_data['doc2']['tokens'] if w not in stopwords]
+        cur_data['doc2']['tokens_without_stopwords'] = [word2ix[w] for w in cur_data['doc2']['tokens'] if w not in stopwords]
         cur_data['doc2']['docid'] = docid
         docid += 1
         datas.append(cur_data)
         datasdict['label'].append(int(fields[0]))
-        datasdict['text_left'].append(fields[5].strip())
-        datasdict['text_right'].append(fields[6].strip())
+        datasdict['text_left'].append(cur_data['doc1']['tokens_without_stopwords'])
+        datasdict['id_left'].append('L-%s' % lineid)
+        datasdict['text_right'].append(cur_data['doc2']['tokens_without_stopwords'])
+        datasdict['id_right'].append('R-%s' % lineid)
     return datas,datasdict
 
 def load_w2v(fin, type, vector_size):
@@ -81,6 +85,12 @@ def load_w2v(fin, type, vector_size):
         sys.exit(1)
     print(type)
     return model
+class newdict(dict):
+    def __missing__(self, key):
+            """Map out-of-vocabulary terms to index 1."""
+            return 1
+        
+    
 def transform_w2v(W2V, vector_size):
     W2V = dict((k, W2V[k]) for k in W2V.keys()
                if len(W2V[k]) == vector_size)
@@ -88,7 +98,7 @@ def transform_w2v(W2V, vector_size):
     W2V_value = [W2V[k] for k in W2V.keys()]
     W2V_VOCAB = W2V.keys()
     W2V_VOCAB = [w for w in W2V_VOCAB]
-    word2ix = {word: i for i, word in enumerate(W2V)}
+    word2ix = newdict({word: i for i, word in enumerate(W2V)})
     return W2V, W2V_VOCAB, word2ix,W2V_value
 def load_W2V_VOCAB(language):
     if language == "Chinese":
